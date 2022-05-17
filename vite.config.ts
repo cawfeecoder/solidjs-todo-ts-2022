5bc9783bd771372a3029531037cca2b982a81c85
cawfeecoder <nick@frushco.de>
2022-05-17 01:01:06 -0400

    github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   plugins: [solidPlugin(), splitVendorChunkPlugin()],
   build: {
     target: 'esnext',
+    outDir: 'docs',
     polyfillDynamicImport: false,
   },
 });
